8fc85296238d1f1fbc5dcb348046ef94383ea7c2
Csontho Marcell <csonthomarci@gmail.com>
2019-05-23 15:14:23 +0200

    Initial Commit

---
+++ Vehicle Counter/Form1.Designer.cs
@@ -0,0 +1,376 @@
+﻿namespace Vehicle_Counter
+{
+    partial class VehicleCounter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.car = new System.Windows.Forms.TextBox();
+            this.minibus = new System.Windows.Forms.TextBox();
+            this.twoaxes = new System.Windows.Forms.TextBox();
+            this.threeaxes = new System.Windows.Forms.TextBox();
+            this.fouraxes = new System.Windows.Forms.TextBox();
+            this.bus = new System.Windows.Forms.TextBox();
+            this.bike = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.bike_button = new System.Windows.Forms.Button();
+            this.bus_button = new System.Windows.Forms.Button();
+            this.fouraxes_button = new System.Windows.Forms.Button();
+            this.threeaxes_button = new System.Windows.Forms.Button();
+            this.twoaxes_button = new System.Windows.Forms.Button();
+            this.minibus_button = new System.Windows.Forms.Button();
+            this.bike_label = new System.Windows.Forms.Label();
+            this.bus_label = new System.Windows.Forms.Label();
+            this.fouraxes_label = new System.Windows.Forms.Label();
+            this.threeaxes_label = new System.Windows.Forms.Label();
+            this.twoaxes_label = new System.Windows.Forms.Label();
+            this.minibus_label = new System.Windows.Forms.Label();
+            this.car_label = new System.Windows.Forms.Label();
+            this.car_button = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // car
+            // 
+            this.car.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.car.Location = new System.Drawing.Point(8, 18);
+            this.car.Name = "car";
+            this.car.ReadOnly = true;
+            this.car.Size = new System.Drawing.Size(69, 20);
+            this.car.TabIndex = 0;
+            this.car.Text = "0";
+            // 
+            // minibus
+            // 
+            this.minibus.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.minibus.Location = new System.Drawing.Point(93, 18);
+            this.minibus.Name = "minibus";
+            this.minibus.ReadOnly = true;
+            this.minibus.Size = new System.Drawing.Size(69, 20);
+            this.minibus.TabIndex = 1;
+            this.minibus.Text = "0";
+            // 
+            // twoaxes
+            // 
+            this.twoaxes.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.twoaxes.Location = new System.Drawing.Point(178, 18);
+            this.twoaxes.Name = "twoaxes";
+            this.twoaxes.ReadOnly = true;
+            this.twoaxes.Size = new System.Drawing.Size(69, 20);
+            this.twoaxes.TabIndex = 2;
+            this.twoaxes.Text = "0";
+            // 
+            // threeaxes
+            // 
+            this.threeaxes.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.threeaxes.Location = new System.Drawing.Point(263, 18);
+            this.threeaxes.Name = "threeaxes";
+            this.threeaxes.ReadOnly = true;
+            this.threeaxes.Size = new System.Drawing.Size(69, 20);
+            this.threeaxes.TabIndex = 3;
+            this.threeaxes.Text = "0";
+            // 
+            // fouraxes
+            // 
+            this.fouraxes.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.fouraxes.Location = new System.Drawing.Point(348, 18);
+            this.fouraxes.Name = "fouraxes";
+            this.fouraxes.ReadOnly = true;
+            this.fouraxes.Size = new System.Drawing.Size(69, 20);
+            this.fouraxes.TabIndex = 4;
+            this.fouraxes.Text = "0";
+            // 
+            // bus
+            // 
+            this.bus.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.bus.Location = new System.Drawing.Point(433, 18);
+            this.bus.Name = "bus";
+            this.bus.ReadOnly = true;
+            this.bus.Size = new System.Drawing.Size(69, 20);
+            this.bus.TabIndex = 5;
+            this.bus.Text = "0";
+            // 
+            // bike
+            // 
+            this.bike.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.bike.Location = new System.Drawing.Point(518, 18);
+            this.bike.Name = "bike";
+            this.bike.ReadOnly = true;
+            this.bike.Size = new System.Drawing.Size(69, 20);
+            this.bike.TabIndex = 6;
+            this.bike.Text = "0";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 7;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28572F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28572F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28572F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28572F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28572F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28572F));
+            this.tableLayoutPanel1.Controls.Add(this.bike_button, 6, 2);
+            this.tableLayoutPanel1.Controls.Add(this.bus_button, 5, 2);
+            this.tableLayoutPanel1.Controls.Add(this.fouraxes_button, 4, 2);
+            this.tableLayoutPanel1.Controls.Add(this.threeaxes_button, 3, 2);
+            this.tableLayoutPanel1.Controls.Add(this.twoaxes_button, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.minibus_button, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.bike_label, 6, 1);
+            this.tableLayoutPanel1.Controls.Add(this.bus_label, 5, 1);
+            this.tableLayoutPanel1.Controls.Add(this.fouraxes_label, 4, 1);
+            this.tableLayoutPanel1.Controls.Add(this.threeaxes_label, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.twoaxes_label, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.minibus_label, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.car, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.bike, 6, 0);
+            this.tableLayoutPanel1.Controls.Add(this.minibus, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.bus, 5, 0);
+            this.tableLayoutPanel1.Controls.Add(this.twoaxes, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.fouraxes, 4, 0);
+            this.tableLayoutPanel1.Controls.Add(this.threeaxes, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.car_label, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.car_button, 0, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 29);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(596, 188);
+            this.tableLayoutPanel1.TabIndex = 7;
+            // 
+            // bike_button
+            // 
+            this.bike_button.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.bike_button.Location = new System.Drawing.Point(515, 126);
+            this.bike_button.Name = "bike_button";
+            this.bike_button.Size = new System.Drawing.Size(75, 48);
+            this.bike_button.TabIndex = 20;
+            this.bike_button.Text = "bicikli";
+            this.bike_button.UseVisualStyleBackColor = true;
+            // 
+            // bus_button
+            // 
+            this.bus_button.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.bus_button.Location = new System.Drawing.Point(430, 126);
+            this.bus_button.Name = "bus_button";
+            this.bus_button.Size = new System.Drawing.Size(75, 48);
+            this.bus_button.TabIndex = 19;
+            this.bus_button.Text = "busz";
+            this.bus_button.UseVisualStyleBackColor = true;
+            // 
+            // fouraxes_button
+            // 
+            this.fouraxes_button.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.fouraxes_button.Location = new System.Drawing.Point(345, 126);
+            this.fouraxes_button.Name = "fouraxes_button";
+            this.fouraxes_button.Size = new System.Drawing.Size(75, 48);
+            this.fouraxes_button.TabIndex = 18;
+            this.fouraxes_button.Text = "négy/több tengelyes";
+            this.fouraxes_button.UseVisualStyleBackColor = true;
+            // 
+            // threeaxes_button
+            // 
+            this.threeaxes_button.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.threeaxes_button.Location = new System.Drawing.Point(260, 126);
+            this.threeaxes_button.Name = "threeaxes_button";
+            this.threeaxes_button.Size = new System.Drawing.Size(75, 48);
+            this.threeaxes_button.TabIndex = 17;
+            this.threeaxes_button.Text = "háromtengelyes";
+            this.threeaxes_button.UseVisualStyleBackColor = true;
+            // 
+            // twoaxes_button
+            // 
+            this.twoaxes_button.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.twoaxes_button.Location = new System.Drawing.Point(175, 126);
+            this.twoaxes_button.Name = "twoaxes_button";
+            this.twoaxes_button.Size = new System.Drawing.Size(75, 48);
+            this.twoaxes_button.TabIndex = 16;
+            this.twoaxes_button.Text = "kéttengelyes";
+            this.twoaxes_button.UseVisualStyleBackColor = true;
+            // 
+            // minibus_button
+            // 
+            this.minibus_button.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.minibus_button.Location = new System.Drawing.Point(90, 126);
+            this.minibus_button.Name = "minibus_button";
+            this.minibus_button.Size = new System.Drawing.Size(75, 48);
+            this.minibus_button.TabIndex = 15;
+            this.minibus_button.Text = "kisbusz";
+            this.minibus_button.UseVisualStyleBackColor = true;
+            // 
+            // bike_label
+            // 
+            this.bike_label.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.bike_label.AutoSize = true;
+            this.bike_label.Location = new System.Drawing.Point(546, 99);
+            this.bike_label.Name = "bike_label";
+            this.bike_label.Size = new System.Drawing.Size(13, 13);
+            this.bike_label.TabIndex = 13;
+            this.bike_label.Text = "6";
+            // 
+            // bus_label
+            // 
+            this.bus_label.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.bus_label.AutoSize = true;
+            this.bus_label.Location = new System.Drawing.Point(461, 99);
+            this.bus_label.Name = "bus_label";
+            this.bus_label.Size = new System.Drawing.Size(13, 13);
+            this.bus_label.TabIndex = 12;
+            this.bus_label.Text = "5";
+            // 
+            // fouraxes_label
+            // 
+            this.fouraxes_label.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.fouraxes_label.AutoSize = true;
+            this.fouraxes_label.Location = new System.Drawing.Point(376, 99);
+            this.fouraxes_label.Name = "fouraxes_label";
+            this.fouraxes_label.Size = new System.Drawing.Size(13, 13);
+            this.fouraxes_label.TabIndex = 11;
+            this.fouraxes_label.Text = "4";
+            // 
+            // threeaxes_label
+            // 
+            this.threeaxes_label.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.threeaxes_label.AutoSize = true;
+            this.threeaxes_label.Location = new System.Drawing.Point(291, 99);
+            this.threeaxes_label.Name = "threeaxes_label";
+            this.threeaxes_label.Size = new System.Drawing.Size(13, 13);
+            this.threeaxes_label.TabIndex = 10;
+            this.threeaxes_label.Text = "3";
+            // 
+            // twoaxes_label
+            // 
+            this.twoaxes_label.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.twoaxes_label.AutoSize = true;
+            this.twoaxes_label.Location = new System.Drawing.Point(206, 99);
+            this.twoaxes_label.Name = "twoaxes_label";
+            this.twoaxes_label.Size = new System.Drawing.Size(13, 13);
+            this.twoaxes_label.TabIndex = 9;
+            this.twoaxes_label.Text = "2";
+            // 
+            // minibus_label
+            // 
+            this.minibus_label.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.minibus_label.AutoSize = true;
+            this.minibus_label.Location = new System.Drawing.Point(121, 99);
+            this.minibus_label.Name = "minibus_label";
+            this.minibus_label.Size = new System.Drawing.Size(13, 13);
+            this.minibus_label.TabIndex = 8;
+            this.minibus_label.Text = "1";
+            // 
+            // car_label
+            // 
+            this.car_label.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.car_label.AutoSize = true;
+            this.car_label.Location = new System.Drawing.Point(36, 99);
+            this.car_label.Name = "car_label";
+            this.car_label.Size = new System.Drawing.Size(13, 13);
+            this.car_label.TabIndex = 7;
+            this.car_label.Text = "0";
+            // 
+            // car_button
+            // 
+            this.car_button.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.car_button.Location = new System.Drawing.Point(5, 126);
+            this.car_button.Name = "car_button";
+            this.car_button.Size = new System.Drawing.Size(75, 48);
+            this.car_button.TabIndex = 14;
+            this.car_button.Text = "autő/motor";
+            this.car_button.UseVisualStyleBackColor = true;
+            this.car_button.Click += new System.EventHandler(this.car_button_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.resetToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(620, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.resetToolStripMenuItem.Text = "Reset";
+            this.resetToolStripMenuItem.Click += new System.EventHandler(this.resetToolStripMenuItem_Click);
+            // 
+            // VehicleCounter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 229);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "VehicleCounter";
+            this.Text = "Vehicle Counter";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.keyPress);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox car;
+        private System.Windows.Forms.TextBox minibus;
+        private System.Windows.Forms.TextBox twoaxes;
+        private System.Windows.Forms.TextBox threeaxes;
+        private System.Windows.Forms.TextBox fouraxes;
+        private System.Windows.Forms.TextBox bus;
+        private System.Windows.Forms.TextBox bike;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label bike_label;
+        private System.Windows.Forms.Label bus_label;
+        private System.Windows.Forms.Label fouraxes_label;
+        private System.Windows.Forms.Label threeaxes_label;
+        private System.Windows.Forms.Label twoaxes_label;
+        private System.Windows.Forms.Label minibus_label;
+        private System.Windows.Forms.Label car_label;
+        private System.Windows.Forms.Button car_button;
+        private System.Windows.Forms.Button bike_button;
+        private System.Windows.Forms.Button bus_button;
+        private System.Windows.Forms.Button fouraxes_button;
+        private System.Windows.Forms.Button threeaxes_button;
+        private System.Windows.Forms.Button twoaxes_button;
+        private System.Windows.Forms.Button minibus_button;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+    }
+}
+
---
+++ Vehicle Counter/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vehicle_Counter
+{
+    public partial class VehicleCounter : Form
+    {
+        public static string car_char = "0";
+        public static string minibus_char = "1";
+        public static string twoaxes_char = "2";
+        public static string threeaxes_char = "3";
+        public static string fouraxes_char = "4";
+        public static string bus_char = "5";
+        public static string bike_char = "6";
+
+        protected int car_n = 0;
+        protected int minibus_n = 0;
+        protected int twoaxes_n = 0;
+        protected int threeaxes_n = 0;
+        protected int fouraxes_n = 0;
+        protected int bus_n = 0;
+        protected int bike_n = 0;
+
+
+        public VehicleCounter()
+        {
+            InitializeComponent();
+        }
+
+        private void keyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar.ToString().Equals(car_char))
+            {
+                car_n++;
+                car.Text = car_n.ToString();
+            }
+
+            if (e.KeyChar.ToString().Equals(minibus_char))
+            {
+                minibus_n++;
+                minibus.Text = minibus_n.ToString();
+            }
+
+            if (e.KeyChar.ToString().Equals(twoaxes_char))
+            {
+                twoaxes_n++;
+                twoaxes.Text = twoaxes_n.ToString();
+            }
+
+            if (e.KeyChar.ToString().Equals(threeaxes_char))
+            {
+                threeaxes_n++;
+                threeaxes.Text = threeaxes_n.ToString();
+            }
+
+            if (e.KeyChar.ToString().Equals(fouraxes_char))
+            {
+                fouraxes_n++;
+                fouraxes.Text = fouraxes_n.ToString();
+            }
+
+            if (e.KeyChar.ToString().Equals(bus_char))
+            {
+                bus_n++;
+                bus.Text = bus_n.ToString();
+            }
+
+            if (e.KeyChar.ToString().Equals(bike_char))
+            {
+                bike_n++;
+                bike.Text = bike_n.ToString();
+            }
+            
+        }
+
+        private void car_button_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void resetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            car_n = 0;
+            minibus_n = 0;
+            twoaxes_n = 0;
+            threeaxes_n = 0;
+            fouraxes_n = 0;
+            bus_n = 0;
+            bike_n = 0;
+
+            car.Text = "0";
+            minibus.Text = "0";
+            twoaxes.Text = "0";
+            threeaxes.Text = "0";
+            fouraxes.Text = "0";
+            bus.Text = "0";
+            bike.Text = "0";
+
+        }
+    }
+}
